ea68f841d83e57e4f111a26aaf7e5b87dbf7175b
yasminaElg <leyayasmina@gmail.com>
2019-11-01 11:54:36 +0100

    bug fix on create-data

--- create-data.js
+++ create-data.js
@@ -1,9 +1,9 @@
 const Models = require('./models/index')
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize({'database':'home_victor', 'username':'postgres', 'password':'root', 'dialect':'postgres'})
+const connection = new Sequelize({'database':'phonebook', 'username':'postgres', 'password':'root', 'dialect':'postgres'})
 connection.sync({ force: true}).then(() => {
-    Models.User.bulkCreate([
+    Models.entry.bulkCreate([
         {firstname: 'Yasmina', lastname: 'El Ghouate', phoneNumber: '+32 02 7777777'},
         {firstname: 'Pierre', lastname: 'Paul', phoneNumber: '+32 02 7777777'},
         {firstname: 'Jacques', lastname: 'Henry', phoneNumber: '+32 02 7777777'}
