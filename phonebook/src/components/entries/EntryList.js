4293cec413d4ce7442937114015a3f77b4ae3f76
yasminaElg <leyayasmina@gmail.com>
2019-11-01 17:13:39 +0100

    added component entryList

---
+++ phonebook/src/components/entries/EntryList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const EntryList = () => {
+  return (
+    <div className="entry-list section">
+        <ul>
+            <li>Firstname, Lastname, phoneNumber</li>
+            <li>Firstname, Lastname, phoneNumber</li>
+            <li>Firstname, Lastname, phoneNumber</li>
+        </ul>
+      
+    </div>
+  )
+}
+
+export const EntryList
--- phonebook/src/components/layout/Navbar.js
+++ phonebook/src/components/layout/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import SignedOutLinks from './SignedOutLinks'
 
 const Navbar = () => {
     return (
         <nav className="nav-wrapper grey darken-3" >
             <div className="container" >
                 <Link to='/' className="brand-logo" >Phonebook</Link>
+                <SignedOutLinks/>
             </div>
         </nav>
     )
