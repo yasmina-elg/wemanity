796eae459be8e96aeddb758b4150eabaa56ba6a5
yasminaElg <leyayasmina@gmail.com>
2019-11-01 13:05:32 +0100

    added a create route to entry router

--- src/routes/EntriesRouter.js
+++ src/routes/EntriesRouter.js
@@ -5,6 +5,7 @@ export const EntriesRouter = () => {
     const router = expressPromiseRouter()
 
     router.get ('/', EntryController.getAll)
+    router.post('/', EntryController.create)
     router.patch('/:id', EntryController.update)
 
     return router
