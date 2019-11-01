63f82bc31e8567de38fbb772df058046b4a9ec02
yasminaElg <leyayasmina@gmail.com>
2019-11-01 12:49:37 +0100

    added update function to entry controller

--- src/controller/EntryController.js
+++ src/controller/EntryController.js
@@ -5,5 +5,35 @@ export const EntryController = {
     getAll: async (req,res) => {
         const entries = await models.entry.findAll({})
         res.status(200).json(entries)
+    },
+
+   
+
+    update: async (req,res) => {
+        try {
+            const entryId = req.params.id
+            const {firstname, lastname, phoneNumber} = req.body
+            await models.entry.update(
+                {
+                    firstname: firstname,
+                    lastname: lastname,
+                    phoneNumber: phoneNumber
+                },
+                {
+                    where: {
+                        id: entryId
+                    }
+                }
+            )
+
+            const entryUpdated = await models.entry.findOne({
+                where: { id: entryId }
+            })
+
+            return res.status(201).json({ 'entry updated': entryUpdated })
+        } catch (error) {
+            res.status(500).json({ 'error': 'unable to update entry' })
+            console.log(error)
+        }
     }
 }
