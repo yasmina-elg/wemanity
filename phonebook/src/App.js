416f0822fec2ad527388f53893e2956955b7e57e
yasminaElg <leyayasmina@gmail.com>
2019-11-01 16:27:28 +0100

    nested navbar in app.js

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
 
 
 class App extends Component {
@@ -7,7 +8,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="App" >
-          <h1>Phonebook</h1>
+          <Navbar/>
         </div>
       </BrowserRouter>
     )
