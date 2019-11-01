a29e9271a88cd6e845d19f9bf0011b514300b5ab
yasminaElg <leyayasmina@gmail.com>
2019-11-01 18:30:34 +0100

    entry form and route to create entry

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import EntryDetails from './components/entries/EntryDetails'
+import CreateEntry from './components/entries/CreateEntry'
 
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Dashboard} />
             <Route exact path='/entries/:id' component={EntryDetails} />
+            <Route exact path='/create' component={CreateEntry} />
           </Switch>
         </div>
       </BrowserRouter>
--- phonebook/src/components/entries/EntryDetails.js
+++ phonebook/src/components/entries/EntryDetails.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const EntryDetails = () => {
+const EntryDetails = (props) => {
+    const id = props.match.params.id
     return (
        <div className="container section entry-details" >
            <div className="card z-depth-0" >
                <div className="card-content" >
-                   <span className="card-title">firstname, lastname</span>
+                   <span className="card-title">firstname, lastname - {id}</span>
                    <span>phoneNumber</span>
                </div>
            </div>
