8b9401346626859adc701138983245067f4956a2
yasminaElg <leyayasmina@gmail.com>
2019-11-01 12:50:04 +0100

    added update route to entry router

--- src/routes/EntriesRouter.js
+++ src/routes/EntriesRouter.js
@@ -5,6 +5,7 @@ export const EntriesRouter = () => {
     const router = expressPromiseRouter()
 
     router.get ('/', EntryController.getAll)
+    router.patch('/:id', EntryController.update)
 
     return router
 }
