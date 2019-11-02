5b3491c717ccfcfd78a177854c52beac9bfdd296
yasminaElg <leyayasmina@gmail.com>
2019-11-02 16:42:24 +0100

    added actual data to entry details

--- phonebook/src/components/entries/EntryDetails.js
+++ phonebook/src/components/entries/EntryDetails.js
@@ -3,16 +3,27 @@ import { connect } from 'react-redux'
 
 const EntryDetails = (props) => {
     const id = props.match.params.id
+    const {entry} = props
+
     return (
        <div className="container section entry-details" >
            <div className="card z-depth-0" >
                <div className="card-content" >
-                   <span className="card-title">firstname, lastname - {id}</span>
-                   <span>phoneNumber</span>
+                   <span className="card-title">{entry.firstname}, {entry.lastname} - {id}</span>
+                   <span>{entry.phoneNumber}</span>
                </div>
            </div>
        </div>
     )
 }
 
-export default connect()(EntryDetails)
+const mapStateToProps = (state, ownProps) => {
+    const id = ownProps.match.params.id
+    const entries = state.entry.entries
+    const entry = entries ? entries.find(entry => entry.id === id) : null
+    return {
+        entry: entry
+    }
+}
+
+export default connect(mapStateToProps)(EntryDetails)
