6cc0a61a73ec3b5d77c1f26527c6202a107f736d
yasminaElg <leyayasmina@gmail.com>
2019-11-02 15:49:09 +0100

    added link to entry details

--- phonebook/src/components/entries/EntryList.js
+++ phonebook/src/components/entries/EntryList.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import EntrySummary from './EntrySummary'
+import { Link } from 'react-router-dom'
 
-const EntryList = ({entries}) => {
+const EntryList = ({ entries }) => {
   return (
     <div className="entry-list section center">
-       { entries && entries.map(entry => {
-         return (
-           <EntrySummary entry={entry} key={entry.id} />
-         )
-       })}
-      
+      {entries && entries.map(entry => {
+        return (
+          <Link to={'/entries/'+ entry.id}>
+            <EntrySummary entry={entry} key={entry.id} />
+          </Link>
+        )
+      })}
+
     </div>
   )
-} 
+}
 
 export default EntryList
